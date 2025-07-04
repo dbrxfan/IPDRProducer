a408db72db6a658f54f3f3ae1be4d2b3b4f8c71a
dbrxfan <pnovoksh@yahoo.com>
2025-07-03 20:14:04 -0400

    Add files via upload
    Initial Commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# Simple Data Generator of IPDR messages
+
+## Main logic:
+1) Generate IPDR record fields. I'm using faker Java library.
+2) Build IPDR JSON message based on the message format listed below.  
+3) Send IPDR message into Kafka queue
+4) Sleep before proceeding to the next message
+
+## Usage on Unsecured Cluster: 
+```
+java -cp IPDRDataProducer.jar data.generator.IPDRDataProducer <bootstrap_server_name:port> <kafka_topic> <number_of_messages_to_generate> <input_rate_milliseconds> <macAddress_start_offset>
+```
+Note: This was tested on Kafka version 2.5.0.7.1.7.1000-142 
+
+Example: to generate 10 messages with sleep time of 1 sec in-between and a distinct mac addresses of MACADDR1 through MACADDR10:
+
+```
+java -cp IPDRProducer.jar data.generator.IPDRDataProducer secondary-5.secondary.root.hwx.site:9092 mytopic 10 1000 0
+ 
+```
+## Usage on Secured SASL_SSL (Kerberos) Cluster:
+
+```
+java -cp IPDRDataProducer.jar data.generator.IPDRDataProducer <bootstrap_server_name:9093> <kafka_topic> <number_of_messages_to_generate> <input_rate_milliseconds> <macAddress_start_offset> <path_to_truststore_file/truststore_file.jks>
+```
+Note: This was tested on Kafka version 3.4.0.7.2.17.0-334 on CDP Public Cloud Data Hub cluster. The code was deployed on the broker node.
+
+Example: to generate 10 messages with sleep time of 1 sec in-between and a distinct mac addresses of MACADDR1 through MACADDR10 on a secured Kafka cluster with the truststore.jks file:
+
+```
+java -cp IPDRProducer.jar data.generator.IPDRDataProducer secondary-5.secondary.root.hwx.site:9093 mytopic 10 1000 0 /home/pnovokshonov/truststore.jks
+```
+## Sample IPDR Message Format:
+
+```
+{"batchId":"103.65.107.58",
+"cmIp":{"v4":"198.75.91.98"},
+"cmMacAddr":"MACADDR39",
+"cmtsHostName":"schaefer.info",
+"cmtsIp":{"v4":"103.65.107.58","v6":"cb20:8db0:c3bf:247b:d593:a9f2:1c2f:50af"},
+"dsChSet":[31,70,79,26],
+"dsIdentifier":5957,
+"dsOctets":635159,
+"dsPackets":6099,
+"dsScn":"default",
+"dsTimeActive":35962,
+"fromTime":"2023-08-01 19:15:00",
+"mdIfIndex":2869,
+"mdIfName":"Cable2/1/6",
+"qosVersion":3,
+"region":"Charlotte",
+"timeZone":"EST",
+"toTime":"2023-08-01 19:30:00",
+"usChSet":[0,6,9,9,8,0,3],
+"usIdentifier":89100,
+"usOctets":584150,
+"usPackets":23106,
+"usScn":"default",
+"usTimeActive":21239,
+"v":2}
+```
+
+## Sample Load Script
+```
+#Load 500,000 IPDR messages to a topic with 0 sec sleep time
+/usr/java/jdk1.8.0_232-cloudera/bin/java -cp IPDRProducer.jar data.generator.IPDRDataProducer pnovokshonov-1.pnovokshonov.root.hwx.site:9092 ipdr_input 100000 0 0   > load1.out 2>&1  &
+/usr/java/jdk1.8.0_232-cloudera/bin/java -cp IPDRProducer.jar data.generator.IPDRDataProducer pnovokshonov-1.pnovokshonov.root.hwx.site:9092 ipdr_input 100000 0 100000   > load2.out 2>&1  &
+/usr/java/jdk1.8.0_232-cloudera/bin/java -cp IPDRProducer.jar data.generator.IPDRDataProducer pnovokshonov-1.pnovokshonov.root.hwx.site:9092 ipdr_input 100000 0 200000   > load3.out 2>&1  &
+/usr/java/jdk1.8.0_232-cloudera/bin/java -cp IPDRProducer.jar data.generator.IPDRDataProducer pnovokshonov-1.pnovokshonov.root.hwx.site:9092 ipdr_input 100000 0 300000   > load4.out 2>&1  &
+/usr/java/jdk1.8.0_232-cloudera/bin/java -cp IPDRProducer.jar data.generator.IPDRDataProducer pnovokshonov-1.pnovokshonov.root.hwx.site:9092 ipdr_input 100000 0 400000   > load5.out 2>&1  &
+```
---
+++ src/main/java/data/generator/IPDRDataProducer.java
@@ -0,0 +1,111 @@
+package data.generator;
+
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.config.SslConfigs;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Properties;
+
+import static data.generator.DataGenerator.buildIPDRMessage;
+import static data.generator.DataGenerator.generateIPDRMessage;
+
+//import util.properties packages
+//import simple producer packages
+//import KafkaProducer packages
+//import ProducerRecord packages
+
+public class IPDRDataProducer {
+    public static void main(String args[]){
+
+        // Check arguments length value
+        if(args.length < 5 || args.length > 6) {
+            System.out.println("Usage on Secured SASL_SSL cluster: java -cp IPDRDataProducer.jar data.generator.IPDRDataProducer " +
+                    "<bootstrap_server_name:9093> <kafka_topic> <number_of_messages_to_generate> <input_rate_ms>" +
+                    "<macAddress_start_offset> <path_to_truststore_file/truststore_file.jks>\n");
+
+            System.out.println("Usage on Unsecured cluster: java -cp IPDRDataProducer.jar data.generator.IPDRDataProducer " +
+                    "<bootstrap_server_name:9092> <kafka_topic> <number_of_messages_to_generate> <input_rate_ms> <macAddress_start_offset>");
+
+            return;
+        }
+
+        //Assign topicName to string variable
+        String bootstrapServer = args[0].toString();
+        String topicName = args[1].toString();
+        Integer numberOfMessagesToGenerate = Integer.valueOf(args[2]);
+        Integer inputRate = Integer.valueOf(args[3]);
+        Integer macAddrStartOffset = Integer.valueOf(args[4]);
+
+
+        // create instance for properties to access producer configs
+        Properties props = new Properties();
+
+        if(args.length > 5 ) {
+            String truststorePathName = args[5];
+           //configure the following three settings for SSL Encryption
+           props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
+           props.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, truststorePathName);
+//            //props.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG,  "test1234");
+
+            //configure the following three settings for Kerberos Authentication
+            props.put("sasl.kerberos.service.name", "kafka");
+            props.put("sasl.jaas.config", "com.sun.security.auth.module.Krb5LoginModule required useTicketCache=true;");
+
+        }
+
+        //Assign localhost id
+        props.put("bootstrap.servers", bootstrapServer);
+
+        //Set acknowledgements for producer requests.
+        props.put("acks", "all");
+
+        //If the request fails, the producer can automatically retry,
+        props.put("retries", 0);
+
+        //Specify buffer size in config
+        props.put("batch.size", 16384);
+
+        //Reduce the no of requests less than 0
+        props.put("linger.ms", 1);
+
+        //The buffer.memory controls the total amount of memory available to the producer for buffering.
+        props.put("buffer.memory", 33554432);
+
+        props.put("key.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+
+        props.put("value.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer
+                <String, String>(props);
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime start_dt = LocalDateTime.now();
+
+        System.out.println("Starting IPDR Data generation at " + dtf.format(start_dt));
+
+        try {
+            for (int i=1; i<=numberOfMessagesToGenerate; i++) {
+                //generateIPDRMessage
+                generateIPDRMessage(i+macAddrStartOffset);
+                //Build an IPDR final JSON string and send it to Kafka topic
+                producer.send(new ProducerRecord<String, String>(topicName, Integer.toString(i), buildIPDRMessage()));
+                //Sleep between getting the next message
+                Thread.sleep(inputRate);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        LocalDateTime end_dt = LocalDateTime.now();
+
+        System.out.println("All IPDR messages sent successfully at "+ dtf.format(end_dt));
+        producer.close();
+
+    }
+}
